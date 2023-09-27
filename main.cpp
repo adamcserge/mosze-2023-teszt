ace96997f5bb208078390d61027c651dd06cfae3
adamcserge <cserge.adam0726@gmail.com>
2023-09-27 13:56:43 +0200

    kommentelt kod

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //rossz változónév 
+    std::cout << '1-100 ertekek duplazasa'  //hiányzik a ;
+    for (int i = 0;)    //hiányos paraméterek
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)   
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"   //csak a szöveget írja ki, az értéket nem, hiányzik a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)    // , helyett ;
     {
-        atlag += b[i]
+        atlag += b[i]   //hiányzik a ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    return 0;  //nincs felszabadítva a memória
 }
