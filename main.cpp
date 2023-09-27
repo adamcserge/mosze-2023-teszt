9d963b0e58c9d8d24c5f1d7292bf0168be357062
adamcserge <cserge.adam0726@gmail.com>
2023-09-27 14:34:33 +0200

    Javított kód

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //rossz változónév 
-    std::cout << '1-100 ertekek duplazasa'  //hiányzik a ;
-    for (int i = 0;)    //hiányos paraméterek
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa"<<std::endl;  
+    for (int i = 0; i<N_ELEMENTS; i++)    
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)   
+    for (int i = 0; i<N_ELEMENTS; i++)   
     {
-        std::cout << "Ertek:"   //csak a szöveget írja ki, az értéket nem, hiányzik a ;
+        std::cout << "Ertek:" <<b[i]; 
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)    // , helyett ;
+    int atlag=0;
+    for (int i = 0; i < N_ELEMENTS; i++)    
     {
-        atlag += b[i]   //hiányzik a ;
+        atlag += b[i];   
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;  //nincs felszabadítva a memória
+    delete[] b;
+    return 0;  
 }
